cf4227c397baf76d6a863f9bc38473f6f3a45876
UBF_Morgan_1 <urielsanta456@gmail.com>
2025-01-17 16:51:10 -0600

    Logro: creacion de una tabla con restricciones e inserciones

--- 01-SQL-LDD.sql
+++ 01-SQL-LDD.sql
@@ -1,1 +1,9 @@
 -- Creacion de base de datos en sql server
+-- En Construccion
+
+
+docker run -e "ACCEPT_EULA=Y" `
+-e "MSSQL_SA_PASSWORD=123456" `
+   -p 1435:1433 --name sqlserver `
+   -d `
+   mcr.microsoft.com/mssql/server:2022-latest
