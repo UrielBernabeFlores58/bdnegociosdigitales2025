b20e895149142fd0057594a2add30528d5ed86cf
UBF_Morgan_1 <urielsanta456@gmail.com>
2025-01-30 17:45:20 -0600

    Logro: Avance de consultas simples con operadores logicos y clausula IN

--- Unidad1/02-creacion_llenado_una_tabla.sql
+++ Unidad1/02-creacion_llenado_una_tabla.sql
@@ -1,13 +1,13 @@
-#Creacion de la base de datos tienda1
+-- Creacion de la base de datos tienda1
 
-#Crea la base de datos tienda1
+-- Crea la base de datos tienda1
 create database tienda1;
 
-#Usar la base de datos
+--Usar la base de datos
 use tienda1;
 
-#SQL-LDD
-#Crear la tabla categoria
+-- SQL-LDD
+--Crear la tabla categoria
 create table categoria(
 categoriaid int not null,
 nombre varchar(20) not null,
@@ -17,8 +17,8 @@ constraint unico_nombre
 unique (nombre)
 );
 
-#SQL-LMD
-#Agregar regristros a la tabla categoria
+-- SQL-LMD
+-- Agregar regristros a la tabla categoria
 insert into categoria
 values (1, 'Carnes frias');
 
@@ -61,6 +61,53 @@ constraint fk_categoria_producto1
 foreign key (categoriaid)
 references categoria (categoriaid)
 );
+
+create table cliente(
+clienteid int not null identity (1, 1),
+codigocliente varchar(15) not null,
+nombre varchar(30) not null,
+direccion varchar (100) not null,
+telefono varchar(19),
+constraint pk_cliente
+primary key (clienteid),
+constraint unico_codigocliente
+unique (codigocliente)
+);
+
+
+create table detalleorden(
+ordenfk int not null,
+productofk int not null,
+preciocompra money not null,
+cantidad int not null,
+constraint pk_detalleorden
+primary key(productofk,preciocompra),
+constraint chk_preciocompra
+check(preciocompra>0.0 and preciocompra<=20000),
+constraint chk_cantidad
+check (cantidad>0),
+constraint fk_detalleorden_producto
+foreign key(productofk)
+references producto1(productoid)
+);
+
+
+create table ordencompra(
+	ordenid int not null identity (1,1),
+	fechadecompra date not null,
+	cliente int not null,
+	constraint pk_ordencompra
+	primary key (ordenid),
+	constraint fk_ordencompra_cliente 
+	foreign key (cliente)
+	references cliente(clienteid)
+);
+
+alter table detalleorden
+add constraint fk_detalleorden_ordencompra
+foreign key (ordenfk)
+references ordencompra(ordenid);
+
 select * from categoria;
 
 
